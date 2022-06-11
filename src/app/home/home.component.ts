0995eb2487504fc690bf1ddf53b0aef6487bfa7b
Danielspv <carlos.developer2001@gmail.com>
2022-06-11 12:49:47 -0500

    Estructura

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+
+import { QuoteService } from './quote.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  quote: string | undefined;
+  isLoading = false;
+
+  constructor(private quoteService: QuoteService) {}
+
+  ngOnInit() {
+    this.quoteService
+      .getRandomQuote({ category: 'dev' })
+      .pipe(finalize(() => {}))
+      .subscribe((quote: string) => {
+        this.quote = quote;
+      });
+  }
+}
